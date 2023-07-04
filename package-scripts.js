a05a13da09ef62023d58f3a3376f71d3472adee3
kilisio <pkilisio@gmail.com>
2023-07-04 14:07:43 +0300

    chore: update package details and readme documentation

--- package-scripts.js
+++ package-scripts.js
@@ -7,17 +7,17 @@ module.exports = {
     rls:
       "npm --no-git-tag-version version " +
       rls.ver +
-      " && git add npm-shrinkwrap.json && git add -p package.json && conventional-changelog -p angular -i CHANGELOG.md -s -r 0 && git add CHANGELOG.md && git commit -m 'Release: v" +
+      " && git add npm-shrinkwrap.json && git add package.json && conventional-changelog -p angular -i CHANGELOG.md -s -r 0 && git add CHANGELOG.md && git commit -m 'Release: v" +
       rls.ver +
       "' && git tag v" +
       rls.ver,
     feat:
       "npm --no-git-tag-version version " +
       feat.ver +
-      " && git add npm-shrinkwrap.json && git add -p package.json && git-cz",
+      " && git add npm-shrinkwrap.json && git add package.json && git-cz",
     fix:
       "npm --no-git-tag-version version " +
       fix.ver +
-      " && git add npm-shrinkwrap.json && git add -p package.json && git-cz",
+      " && git add npm-shrinkwrap.json && git add package.json && git-cz",
   },
 };
